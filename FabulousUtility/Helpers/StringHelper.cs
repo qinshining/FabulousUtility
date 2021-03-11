20fb732a42c8613aa776481b526d7a157314ed29
qinshining <ederqin@qq.com>
2021-03-12 00:12:56 +0800

    1. 2.0环境下能使用扩展方法；2. 添加String的一些扩展方法

---
+++ FabulousUtility/Helpers/StringHelper.cs
@@ -0,0 +1,41 @@
+﻿namespace System
+{
+    public static class StringHelper
+    {
+        public static string SafeToLower(this string This)
+        {
+            if (This != null)
+            {
+                return This.ToLower();
+            }
+            return string.Empty;
+        }
+
+        public static string SafeToUpper(this string This)
+        {
+            if (This != null)
+            {
+                return This.ToUpper();
+            }
+            return string.Empty;
+        }
+
+        public static string SafeReplace(this string This, char oldChar, char newChar)
+        {
+            if (This != null)
+            {
+                return This.Replace(oldChar, newChar);
+            }
+            return string.Empty;
+        }
+
+        public static string SafeReplace(this string This, string oldValue, string newValud)
+        {
+            if (This != null)
+            {
+                return This.Replace(oldValue, newValud);
+            }
+            return string.Empty;
+        }
+    }
+}
