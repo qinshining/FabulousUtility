a3e69f84fb7c4e4e3219a75415c5b53f2e45c8ad
qinshining <ederqin@qq.com>
2021-03-11 23:59:15 +0800

    添加项目文件。

---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+Some useful C# utils method and library.
+
+Mainly written by .net2.0 for first Version. 
+
+Maybe later update for high version.
+
+Do it Slowly perfect.
+
+
+
---
+++ TestDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
